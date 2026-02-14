6b7dac87fc79f19903264360149a1cbb0ea260c3
leo-grnd <leogarnaud84@gmail.com>
2026-02-14 10:44:58 +0100

    Update raptor-kt dependency to version 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,7 +103,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.geometry)
 
     // Raptor-KT
-    implementation("eu.dotshell:raptor-kt:1.4.0")
+    implementation("eu.dotshell:raptor-kt:1.5.0")
 
     // Kotlinx Serialization for fast JSON caching
     implementation(libs.kotlinx.serialization.json)
